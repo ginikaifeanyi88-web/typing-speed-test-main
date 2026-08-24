1dc5cbfbf74b6d922f33008860e7f3b3d6fb5a26
Ginika Okeke <243939796+ginikaifeanyi88-web@users.noreply.github.com>
2026-08-24 10:36:14 +0200

    commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,5 @@
 import jsonData from './data/data.json' with { type: "json" };
 console.log(jsonData);
+
+const textWall = document.querySelector(".text-wall");
+textWall.innerHTML =  jsonData.easy[5].text;
